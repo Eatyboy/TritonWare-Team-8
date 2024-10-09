846b7a4614228354c1eabc8057fb352903ab9b1d
Eatyboy <141588549+Eatyboy@users.noreply.github.com>
2024-10-08 21:17:34 -0700

    added player object with movement, added new input system, added canvas

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    private InputActions ctrl;
+    [SerializeField] private Rigidbody2D rb;
+
+    [SerializeField] private float moveSpeed;
+
+    // Start is called before the first frame update
+    void Awake() {
+        ctrl = new InputActions();
+    }
+
+    private void OnEnable() {
+        ctrl.Enable();
+        ctrl.Gameplay.Ability1.performed += Ability1;
+        ctrl.Gameplay.Ability2.performed += Ability2;
+    }
+
+    private void OnDisable() {
+        ctrl.Gameplay.Ability1.performed -= Ability1;
+        ctrl.Gameplay.Ability2.performed -= Ability2;
+        ctrl.Disable();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float dt = Time.deltaTime;
+    }
+
+    private void FixedUpdate() {
+        float fdt = Time.fixedDeltaTime;
+        Vector2 movement = ctrl.Gameplay.Move.ReadValue<Vector2>();
+
+        rb.position += moveSpeed * fdt * movement;
+    }
+
+    private void Ability1(InputAction.CallbackContext ctx) {
+        
+    }
+
+    private void Ability2(InputAction.CallbackContext ctx) {
+    }
+}
